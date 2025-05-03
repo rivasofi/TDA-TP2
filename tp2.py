7a85b428e6862143ff55d07960758b12765770c9
BrendaBarletta <bbarletta@fi.uba.ar>
2025-05-03 13:10:40 -0300

    Modifico tp2.py para que brinde más información del reusltado obtenido

--- tp2.py
+++ tp2.py
@@ -8,15 +8,30 @@ def main():
     
     posibles_palabras = cargar_palabras(archivo_listado)
     mensajes_a_analizar = cargar_mensajes(archivo_mensajes)
-    
+
+    cant_lineas_invalidas = 0
+    lineas_invalidas = []
+    contador_lineas = 1
+
     for linea in mensajes_a_analizar:
         opt, indices = algoritmo(linea, posibles_palabras)
         
         if not opt[len(linea)]:
-            print("No es un mensaje")
+            print("\033[31mNo es un mensaje\033[0m")
+            cant_lineas_invalidas += 1
+            lineas_invalidas.append(contador_lineas)
+
         else:
             palabras = reconstruccion(linea, indices, len(linea))
-            print(" ".join(palabras))
+            for palabra in palabras:
+                print(palabra, end=" ")
+            print()  # para el salto de línea
+
+        contador_lineas += 1
+
+    print(f"\nCantidad de líneas del archivo: {contador_lineas - 1}")
+    print(f"Cantidad de líneas inválidas del archivo: {cant_lineas_invalidas}")
+    print(f"Líneas inválidas: {lineas_invalidas}")
     
 if __name__ == "__main__":
     main()
